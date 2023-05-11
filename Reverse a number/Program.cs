c37c37fa6af1326b7f77f1b15b91bb9caa73ca7d
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-11 21:22:51 +0530

    Reverse a number

---
+++ Reverse a number/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse_a_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int reversedNum = 0, remainder = 0;
+
+            while (num != 0)
+            {
+                remainder = num % 10;
+                reversedNum = (reversedNum * 10) + remainder;
+                num /= 10;
+            }
+
+            Console.WriteLine($"Reversed Number: {reversedNum}");
+        }
+    }
+}
