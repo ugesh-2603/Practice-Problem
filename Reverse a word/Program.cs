0a8d0e1a103a7f9bee2d6893758e62e81a4a8e64
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-11 21:37:32 +0530

    Reverse a Word

---
+++ Reverse a word/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Reverse_a_word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word, reversedWord = "";
+
+            Console.Write("Enter a word: ");
+            word = Console.ReadLine();
+
+            for (int i = word.Length - 1; i >= 0; i--) 
+            {
+                reversedWord += word[i];
+            }
+
+            Console.WriteLine("The reversed word is: " + reversedWord);
+        }
+    }
+}
