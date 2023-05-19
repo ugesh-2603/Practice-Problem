8763c0cc9acb3f5e2c4cb8d62304e69e412cc88d
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-19 08:46:39 +0530

    Prime Numbers

---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Prime_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = true;
+
+            if (number <= 1)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                // Loop from 2 to the square root of the number
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    // If the number is divisible by any other number, it is not prime
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine(number + " is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a prime number.");
+            }
+        }
+    }
+}
