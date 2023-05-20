e4ff28eff48d1606aea860947f2bb4809d594e4a
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-20 07:38:44 +0530

    Stop Watch Program

---
+++ Stop Watch Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Stop_Watch_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start the stopwatch...");
+            Console.ReadKey();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine("Stopwatch started. Press any key to stop...");
+            Console.ReadKey();
+
+            stopwatch.Stop();
+
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+
+            Console.WriteLine("Elapsed Time: {0}", elapsedTime);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
