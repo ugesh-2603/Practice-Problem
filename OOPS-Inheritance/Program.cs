06cf7ae811927d49891dd82b337b100973aac8c5
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-24 07:51:25 +0530

    OOPS-Inheritance

---
+++ OOPS-Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace OOPS_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance is the process of acquiring all the properties of one class into another class.");
+
+            // Creating an instance of the derived class
+            Dog dog = new Dog();
+
+            // Calling methods from the base class
+            dog.Eat();
+            dog.Sleep();
+
+            // Calling method from the derived class
+            dog.Bark();
+
+            Console.ReadLine();
+        }
+    }
+
+    // Base class
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Animal is sleeping.");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking.");
+        }
+    }
+}
